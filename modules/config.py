abc0de57821a2b50e1019572ff5e815053f88bb5
hikuohiku <hikuohiku@gmail.com>
2025-05-11 01:42:41 +0900

    example追加

--- modules/config.py
+++ modules/config.py
@@ -7,6 +7,10 @@ class Config(BaseSettings):
     firefox_profile_path: DirectoryPath = Field(
         default=...  # linterのエラー抑制のため https://github.com/pydantic/pydantic-settings/issues/201
     )
-    mf_accounts_url: HttpUrl = Field(default=...)
-    mf_refresh_xpath: str = Field(default=...)
-    mf_refresh_button_text: str = Field(default=...)
+    mf_accounts_url: HttpUrl = Field(
+        default=HttpUrl("https://moneyforward.com/accounts")
+    )
+    mf_refresh_xpath: str = Field(
+        default="/html/body/div[1]/div[2]/div[1]/div/div/div/section[2]/p[2]/a"
+    )
+    mf_refresh_button_text: str = Field(default="金融機関からのデータ一括更新")
